0378c475bf1d132125b80aa55cc7da62d6ab3ae2
Leonardo Moura de Sousa <leonardomourass2008@gmail.com>
2026-04-27 20:21:53 -0300

    Dashboard começada

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -15,6 +15,24 @@ function validarSessao() {
   }
 }
 
+function validarSessaoDash() {
+  var email = sessionStorage.EMAIL_USUARIO;
+  var nome = sessionStorage.NOME_USUARIO;
+
+  var b_usuario = document.getElementById("nome_usuario");
+  var paragrafo = document.getElementById("paragrafo-ao-logar");
+  var linkLogin = document.getElementById("link-login");
+
+  if (email != null && nome != null) {
+    var finalDoPrimeiroNome = nome.indexOf(" ");
+    b_usuario.innerHTML = `${nome.substring(0, finalDoPrimeiroNome)}!`;
+    paragrafo.style.display = "inline-block";
+    linkLogin.style.display = "none";
+  } else {
+    window.location = "./login.html"
+  }
+}
+
 function limparSessao() {
   sessionStorage.clear();
   window.location = "./pages/login.html";
